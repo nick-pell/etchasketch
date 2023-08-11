9b556a031eae80751ed43340d444b3b709cd651c
nick-pell <93624627+nick-pell@users.noreply.github.com>
2023-08-11 15:13:11 -0400

    First update
    Added grid

---
+++ script.js
@@ -0,0 +1,21 @@
+let gridWidth = 16;
+let gridHeight = 16;
+
+const grid = document.querySelector('.grid');
+
+function drawGrid(gridWidth, gridHeight){
+
+for(let i = 0; i < gridHeight; i++){
+    const row = document.createElement('div');
+    row.classList.add('row');
+    grid.appendChild(row);
+    for(let j = 0; j < gridWidth; j++){
+        const box = document.createElement('div');
+        box.classList.add('box');
+        row.appendChild(box);
+    }
+}
+
+}
+
+drawGrid(gridWidth,gridHeight);
